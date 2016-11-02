de07c42e8639f603efdab9463c369b174c4634d1
Mike Covington <mfcovington@gmail.com>
2016-11-02 14:29:02 -0700

    Re-organize URL structure

--- compatible_index_sequences/urls.py
+++ compatible_index_sequences/urls.py
@@ -6,8 +6,8 @@ from .views import (
 
 
 urlpatterns = [
-    url(r'^$', IndexSetListView.as_view(), name='index_set_list'),
+    url(r'^custom/$', check_custom_index_list, name='check_custom_index_list'),
+    url(r'^custom/results/$', custom_index_list_results, name='custom_index_list_results'),
     url(r'^index_set/(?P<pk>\d+)/$', IndexSetDetailView.as_view(), name='index_set_detail'),
-    url(r'^check_list/$', check_custom_index_list, name='check_custom_index_list'),
-    url(r'^check_list/results/$', custom_index_list_results, name='custom_index_list_results'),
+    url(r'^interactive/$', IndexSetListView.as_view(), name='index_set_list'),
 ]
