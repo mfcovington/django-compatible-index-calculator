b703518eb09e83f08b17fe1041fae37b182bd338
Mike Covington <mfcovington@gmail.com>
2016-11-03 13:20:12 -0700

    Raise validation error unless index sequences are entered or sample sheet is uploaded

--- compatible_index_sequences/forms.py
+++ compatible_index_sequences/forms.py
@@ -22,3 +22,12 @@ class CustomIndexListForm(forms.Form):
         validators=[validators.MaxLengthValidator(0)],
         widget=forms.HiddenInput,
     )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        index_list = cleaned_data.get('index_list')
+        samplesheet = cleaned_data.get('samplesheet')
+
+        if not index_list and not samplesheet:
+            raise forms.ValidationError(
+                'Please enter index sequences and/or upload a sample sheet.')
